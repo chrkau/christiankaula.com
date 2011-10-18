39a3da47996eba2fa4167da1ce6a60f0c08f839e
Christian Kaula <ck@10tok.net>
2011-10-18 09:09:52 +0200

    some code cleanups

--- cache_tweets.py
+++ cache_tweets.py
@@ -1,9 +1,31 @@
 #!/usr/bin/env python
 
+# Copyright (c) 2011, Christian Kaula
+# All rights reserved.
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions are met:
+#
+# * Redistributions of source code must retain the above copyright notice, this
+#   list of conditions and the following disclaimer.
+# * Redistributions in binary form must reproduce the above copyright notice,
+#   this list of conditions and the following disclaimer in the documentation
+#   and/or other materials provided with the distribution.
+#
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+# AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+# IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+# ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+# LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+# CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+# SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+# INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+# CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+# ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+# POSSIBILITY OF SUCH DAMAGE.
+
 import json
 import logging
-import os
-import sys
 from codecs import open
 from datetime import datetime
 from rfc822 import parsedate
@@ -29,7 +51,9 @@ fetch_url = 'https://api.twitter.com/1/statuses/user_timeline.json?%s' % params
 logging.info('accessing %s' % fetch_url)
 urlhandle = urlopen(fetch_url)
 data = json.loads(unicode(urlhandle.read()))
-print(len(data))
+
+
+### handling
 
 tweets = []
 for tweet in data[:4]:
@@ -49,7 +73,6 @@ for tweet in data[:4]:
 		)
 
 	tweets.append({
-		# 'date': timestamp.strftime('%Y-%m-%d %H:%M'),
 		'date': timestamp.strftime('%c'),
 		'text': text,
 	})

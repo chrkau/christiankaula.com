6f35510b8f967289a46c546b7ab04ceaef4b249a
Christian Kaula <ck@10tok.net>
2012-11-01 10:29:21 +0100

    added some author settings

--- pelicanconf.py
+++ pelicanconf.py
@@ -1,4 +1,24 @@
 AUTHOR = u'Christian Kaula'
+AUTHOR_EMAIL = u'ck@christiankaula.com'
+AUTHOR_XMPP = u'chris@10tok.net'
+AUTHOR_TWITTER = u'chrkau'
+AUTHOR_PHONE = u'+49 179 9709833'
+AUTHOR_STREET = u'Thieraufstrasse 2'
+AUTHOR_ZIP = u'95028'
+AUTHOR_CITY = u'Hof'
+AUTHOR_COUNTRY = u'Germany'
+
+SOCIAL = (
+	(u'https://www.xing.com/profile/Christian_Kaula', u'Xing'),
+	(u'http://twitter.com/chrkau', u'Twitter'),
+	(u'xmpp:chris@10tok.net', u'XMPP'),
+	(u'https://github.com/chrkau', u'GitHub'),
+	(u'https://bitbucket.org/chrkau', u'BitBucket'),
+	# (u'http://djangopeople.net/ckau/', u'Django People'),
+	(u'http://djangogigs.com/developers/christian-kaula/', u'Djangogigs'),
+)
+
+
 SITENAME = u'Christian Kaula'
 SITESUBTITLE = u'I make stuff simple.'
 SITEURL = u'http://christiankaula.com'
@@ -11,4 +31,7 @@ DEFAULT_PAGINATION = 5
 THEME = 'theme/ck03.5'
 
 TYPOGRIFY = True
+
+PIWIK_URL = 'piwik.10tok.net'
+PIWIK_SITE_ID = 1
 
